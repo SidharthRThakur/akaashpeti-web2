2e3ffbccd8d61378986d771ea9f65f25457a3489
Sid-Thakur <tsidharthsingh@gmail.com>
2025-09-08 23:57:18 +0530

    Replace file/pagets

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
